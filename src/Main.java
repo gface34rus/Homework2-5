7064e4113fa04bf99d5756151704eeaff781299d
gface34rus <89259112970@mail.ru>
2024-08-04 12:50:22 +0300

    Homework is fixed 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+import validations.DataValidator;
+
 import static validations.DataValidator.validateData;
 
 public class Main {
@@ -5,6 +9,10 @@ public class Main {
         String login = "java_skypro_go";
         String password = "D_1hWiKjjP_9";
         String confirmPassword = "D_1hWiKjjP_9";
-        validateData(login, password, confirmPassword);
+        try {
+            validateData(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Поймано исключение: " + e.getMessage());
+        }
     }
 }
--- src/validations/DataValidator.java
+++ src/validations/DataValidator.java
@@ -5,18 +5,14 @@ import exceptions.WrongPasswordException;
 
 public class DataValidator {
     public static void validateData(String login, String password, String confirmPassword) {
-        try {
-            if (login.length() > 20 || !login.matches("^[a-zA-Z0-9_]{1,20}$")) {
-                throw new WrongLoginException("Неверный формат логина! Используйте латинские буквы цифры 0-9 и/или символ _");
-            }
-            if (password.length() > 20 || !password.matches("^[a-zA-Z0-9_]{1,20}$")) {
-                throw new WrongPasswordException("Неверный формат пароля! Используйте латинские буквы цифры 0-9 и/или символ _");
-            }
-            if (!password.equals(confirmPassword)) {
-                throw new WrongPasswordException("Пароли не совпадают!");
-            }
-        } catch (WrongLoginException | WrongPasswordException e) {
-            System.out.println("Поймано исключение: " + e.getMessage());
+        if (!login.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            throw new WrongLoginException("Неверный формат логина! Используйте латинские буквы цифры 0-9 и/или символ _");
+        }
+        if (!password.matches("^[a-zA-Z0-9_]{1,20}$")) {
+            throw new WrongPasswordException("Неверный формат пароля! Используйте латинские буквы цифры 0-9 и/или символ _");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают!");
         }
     }
 }
